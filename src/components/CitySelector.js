d2fca5626072308b092a34e949ad73d8f83ee89e
Yaw Teh <yawteh@gmail.com>
2021-03-26 23:50:22 -0400

    Added City Selector Component

---
+++ src/components/CitySelector.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Row, Col, FormControl, Button } from 'react-bootstrap';
+
+const CitySelector = ({onSearch}) => {
+    const [city, setCity] = useState('');
+
+    return (
+        <div>
+            <Row>
+                <Col>
+                    <h1>Search your city</h1>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col xs={4} className="text-center">
+                    <FormControl
+                    placeholder = "Enter City"
+                    onChange={(event) => setCity(event.target.value)}
+                    value={city}
+                    />
+                </Col>
+            </Row>
+            
+            <Row>
+                <Col>
+                    <Button onClick={() => onSearch(city)}>Check Weather</Button>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+
+
+export default CitySelector;
