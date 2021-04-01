999b6dfd10eea3035a5994844552272fae99f627
Yaw Teh <yawteh@gmail.com>
2021-03-31 23:34:38 -0400

    Weather App Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import Weathercard from './components/WeatherCard';
+import CitySelector from './components/CitySelector';
+import UseFetch from './hooks/UseFetch';
+import { Container } from 'react-bootstrap';
+import { API_KEY, API_BASE_URL } from './apis/config'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
 
+
+const App = () => {
+  const { data, error, isLoading, setUrl } = UseFetch();
+
+  
+    return (
+      <Container className="App">
+        <CitySelector
+        onSearch={(city) => setUrl(`${API_BASE_URL}/data/2.5/forecast?q=${city}&cnt=5&appid=${API_KEY}`)} 
+        />
+      </Container>
+    );
+  };
+  
 export default App;
